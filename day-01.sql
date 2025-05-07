3106c20c7c1c6913ebd79ade9724b3661fd79403
Danziel Cempron <104302220+Dnzldotexe@users.noreply.github.com>
2025-05-07 16:07:33 +0800

    Create day-01.sql

---
+++ day-01.sql
@@ -0,0 +1,28 @@
+-- distinct districts
+-- SELECT DISTINCT
+-- 	district
+-- FROM
+-- 	address
+-- ORDER BY
+-- 	district
+
+-- latest rental date
+-- SELECT
+-- 	rental_date
+-- FROM
+-- 	rental
+-- ORDER BY
+-- 	rental_date DESC
+-- LIMIT 1
+
+-- how many films does the company have
+-- SELECT DISTINCT
+-- 	COUNT(film_id)
+-- FROM
+-- 	film
+
+-- how many distinct last names of the customers
+-- SELECT DISTINCT
+-- 	COUNT(last_name)
+-- FROM
+-- 	customer
